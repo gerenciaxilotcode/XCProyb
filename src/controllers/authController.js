a91202137c762ba361cc54a60d025064ce62b399
Ángel López Martínez <angelserilopezmartinez@gmail.com>
2026-08-22 23:05:03 -0600

    5 commit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,9 +6,10 @@ import { env } from '../config/env.js'
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: env.isProduction,
-  sameSite: env.isProduction ? 'none' : 'lax',
-  maxAge: 7 * 24 * 60 * 60 * 1000
+  secure: true,
+  sameSite: 'none',
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: '/'
 }
 
 export async function login(req, res) {
